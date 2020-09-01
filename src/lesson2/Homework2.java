24234c80f4f073e552cd57229f7500b1bc5ebef5
Lara <tatasemon@mail.ru>
2020-09-02 01:41:51 +0500

    hw2- методы к задачам прописаны, но видимо  прописывания  общих подпрограмм не верно??

--- src/lesson2/Homework2.java
+++ src/lesson2/Homework2.java
@@ -1,24 +1,90 @@
 package lesson2;
 
+import org.jetbrains.annotations.NotNull;
+import sun.security.smartcardio.SunPCSC;
+
+import javax.xml.stream.FactoryConfigurationError;
 import java.util.Arrays;
 
 public class Homework2 {
     public static void main(String[] args) {
+        int i;
 
-        int sample[] = new int[]{1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        printTask((int) 1);
+        int[] arr = new int[8];
         String[] strArr = new String[10];
+        printArr(stepByStep(8);
+        FillArr(arr);
+        printArr(factoryArr( 12));
+    }
 
-        for (int i = 0; i < sample.length; i++) {
-            System.out.println(sample[i] + " ");
 
-            sample[i] ++;
+
+    public static void main(String[] args){
+
+            int sample[] = new int[]{1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+
+
+            for (int i = 0; i < sample.length; i++) {
+                if (sample[i] == 0) {
+                    sample[i] = 1;
+                } else sample[i] = 0;
+            }
+            System.out.println(Arrays.toString(sample));
+            }
+
+
+            public static void stepByStep ( int[] arr){
+
+
+                String[] stringsArr = new String[8];
+
+                for (int i = 0; i < arr.length; i++) {
+                    arr[i] *= 3;
+                }
+                System.out.println(Arrays.toString(new String[]{arr[i] + " "}));
+
+//public static void FillArr(int[] arr) {
+//    for (int i = 1; i < arr.length; i++) {
+//        arr[i] = arr[i - 1] + 3;
+           }
+
+
+        public static int[][] factoryArr ( int n){
+            int[][] arr = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                arr[i][i] = 1;
+                arr[i][n - 1 - i] = 1;
+            }
+            return arr;
         }
 
+        public static int maxValue ( int[] arr){
+            int max = arr[0];
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] > max) {
+                    max = arr[i];
+                }
+            }
+            return max;
+        }
 
-
-        System.out.println(Arrays.toString(sample));
+    public static boolean checkBalance(int[] arr) {
+        int summ = 0;
+        for (int i = 0; i < arr.length; i++) {
+            summ += arr[i]; /*находим сумму всех элементов из задачи №6 дз№2*/
+        }
+        if (summ % 2 != 0) {
+            return false;
+        }
+        int sumLeft = 0;
+        int i = 0;
+        int summLeft;
+        while (sumLeft < summ / 2) {
+            summLeft += arr[i];
+            i++;
+        }
+        return summLeft == summ / 2;
     }
 }
-
-
 
